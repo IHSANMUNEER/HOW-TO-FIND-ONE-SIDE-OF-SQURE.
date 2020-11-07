b755ca99cb132fdc31bc976c6b65f13df25a85e1
IHSAN MUNEER <72526107+IHSANMUNEER@users.noreply.github.com>
2020-11-07 12:59:12 +0500

    one side squre by using function

---
+++ one side of area by using function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float one(float a);//function decleration
+int main()//main function
+{float a,b;
+    printf("Enter the area:");
+scanf("%f",&a);
+    one(a);//function call
+    return b;
+}
+float one(float a)//function define
+ {float b;
+     printf("one side is =%f",b=a/4);
+    return 0;}
